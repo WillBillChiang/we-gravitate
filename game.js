ebc97f4f6d06e6bcfa0dfcca9993b03074ccdf24
William Chiang <45242555+WillBillChiang@users.noreply.github.com>
2020-08-02 01:30:46 -0400

    Added art and minor updates

--- game.js
+++ game.js
@@ -34,10 +34,23 @@ var nameDict = {'s': "Sun", 'r': "Rocket", 'z': "Mercury", 'x': "Venus", 'c': "E
 var massState = 1.989 * 10 ** 30;
 //current mass of object
 
-var massDict = {'s': 1.989 * 10 ** 30, 'r':0, 'z':0, 'x': 0, 'c': 5.972 * 10 ** 24, 'v':0, 'b':0, 'n':0, 'm':0, 'k':0}
+var massDict = {'s': 1.989 * 10 ** 30, 'r':10 ** 3, 'z': 3.29 * 10 ** 23, 'x': 4.87 * 10 ** 24, 'c': 5.972 * 10 ** 24, 'v': 6.39 * 10 ** 23, 'b': 1.898 * 10 ** 27, 'n': 5.683 * 10 ** 26, 'm': 8.681 * 10 ** 25, 'k': 1.024 * 10 ** 26}
 // all masses are currently set to 0 Kg
 
-function preload() {}
+var imageDict = {'s': "art/sun.png", 'r': "art/rocket.png", 'z': "art/mercury.png", 'x': "art/venus.png", 'c': "art/earth.png", 'v': "art/mars.png", 'b': "art/jupiter.png", 'n': "art/saturn.png", 'm': "art/uranus.png", 'k': "art/nepu"}
+
+function preload() {
+  this.load.image("Sun", "art/sun.png")
+  this.load.image("Rocket", "art/rotrocket.png")
+  this.load.image("Mercury", "art/mercury.png")
+  this.load.image("Venus", "art/venus.png")
+  this.load.image("Earth", "art/earth.png")
+  this.load.image("Mars", "art/mars.png")
+  this.load.image("Jupiter", "art/jupiter.png")
+  this.load.image("Saturn", "art/saturn.png")
+  this.load.image("Uranus", "art/uranus.png")
+  this.load.image("Neptune", "art/neptune.png")
+}
 
 function create() {
   planets = this.add.group();
@@ -47,6 +60,8 @@ function create() {
   this.keys = this.input.keyboard.createCursorKeys();
 }
 
+// Note: "planet" refers to everything, including the sun and rocket
+
 function update() {
   // Accepts input to place planet
   if (game.input.activePointer.isDown && !waitForClick && dumbCount > 10) {
@@ -54,6 +69,12 @@ function update() {
     let y = game.input.mousePointer.y;
     let planet = this.add.image(0, 0, nameDict[state]);
     planets.add(planet);
+    planet.setScale(0.15);
+    if (state === 'r') {
+      planet.rocket = true
+    } else {
+      planet.rocket = false
+    }
     // planet.fillStyle(0xFaa23, 1);
     // planet.fillCircle(0, 0, 10);
     planet.speed = new Phaser.Math.Vector2(0, 0);
@@ -77,10 +98,10 @@ function update() {
 
   if (!waitForClick) {
     let allPlanets = planets.getChildren();
+    var accXplanets = []
+    var accYplanets = []
     for (let ind = 0; ind < allPlanets.length; ind += 1) {
       // Adjusts position based on current velocity
-      allPlanets[ind].x += allPlanets[ind].speed.x/10**13;
-      allPlanets[ind].y += allPlanets[ind].speed.y/10**13;
       x = allPlanets[ind].x;
       y = allPlanets[ind].y;
       accX = 0;
@@ -93,7 +114,6 @@ function update() {
         if (!(x == x2 && y == y2)) {
           accTot = (6.67*10**-11*mass)/Math.sqrt(((x-x2))**2 + ((y-y2))**2);
           angle = Math.atan(Math.abs(x-x2)/Math.abs(y-y2));
-          console.log("Acceleration:" + accTot)
           if (x2 < x) {
             accX -= Math.cos(angle)*accTot;
           } else if (x2 > x) {
@@ -107,8 +127,38 @@ function update() {
           }
         }
       }
-      allPlanets[ind].speed.x += accX*10**(-6)
-      allPlanets[ind].speed.y += accY*10**(-6)
+      accXplanets.push(accX*10**(-5.7))
+      accYplanets.push(accY*10**(-5.7))
+
+    }
+
+    if (accXplanets.length != 0) {
+    console.log("--")
+    console.log(accXplanets)
+    console.log(accYplanets)}
+
+    // Adjusts Velocity
+    for (let ind = 0; ind < accXplanets.length; ind += 1) {
+      allPlanets[ind].speed.x += accXplanets[ind]
+      allPlanets[ind].speed.y += accYplanets[ind]
+    }
+
+    // Adjusts Position
+    for (let ind = 0; ind < accXplanets.length; ind += 1) {
+      allPlanets[ind].x += allPlanets[ind].speed.x/10**13
+      allPlanets[ind].y += allPlanets[ind].speed.y/10**13
+    }
+    
+    // Adjust Rocket Rotation
+    for (let ind = 0; ind < allPlanets.length; ind += 1) {
+      if (allPlanets[ind].rocket) {
+        x = allPlanets[ind].speed.x; y = allPlanets[ind].speed.y;
+        allPlanets[ind].angle = Math.atan2(allPlanets[ind].speed.y,allPlanets[ind].speed.x)*180/Math.PI
+        if (allPlanets[ind].angle < 0) {
+          allPlanets[ind].angle += 360
+        }
+        allPlanets[ind].angle += 45
+      }
     }
   }
 
